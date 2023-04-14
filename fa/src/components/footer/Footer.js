898b792220c6a677c2ddfc9c9f9482780c1d6a07
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-04-14 19:22:13 +0200

    space

--- fa/src/components/footer/Footer.js
+++ fa/src/components/footer/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-
 import { ReactComponent as DiscordSVG } from '../../icons/Discord.svg'
 import { ReactComponent as TwitterSVG } from '../../icons/Twitter.svg'
 import { ReactComponent as InstagramVG } from '../../icons/Instagram.svg'
-
 import { ReactComponent as OpenSeaSVG } from '../../icons/OpenSea.svg'
-
 import './footer.css'
+
 const Footer = () => {
   const handleDiscord = () => {
     window.open('')
